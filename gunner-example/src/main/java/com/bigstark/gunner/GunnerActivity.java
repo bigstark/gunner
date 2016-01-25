d507eb0f089a4166f9a445f5cec7f9533e95207d
BigStarK <bigstark@makeus.com>
2016-01-25 19:38:00 +0900

    update gunner example

--- gunner-example/src/main/java/com/bigstark/gunner/GunnerActivity.java
+++ gunner-example/src/main/java/com/bigstark/gunner/GunnerActivity.java
@@ -3,41 +3,64 @@ package com.bigstark.gunner;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 import com.bigstark.gunner.library.Bullet;
 import com.bigstark.gunner.library.Gunner;
 
 public class GunnerActivity extends Activity {
 
+    private ImageView imageview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gunner);
-        shoot();
+        imageview = (ImageView) findViewById(R.id.imageview);
     }
 
-    private void shoot() {
+
+    @Bullet(sequence = 1, delay = 500)
+    private void load1() {
+        imageview.setImageResource(R.drawable.android1);
+    }
+
+    @Bullet(sequence = 2, delay = 500)
+    private void load2() {
+        imageview.setImageResource(R.drawable.android2);
+    }
+
+    @Bullet(sequence = 3, delay = 500)
+    void load3() {
+        imageview.setImageResource(R.drawable.android3);
+    }
+
+    @Bullet(sequence = 4, delay = 500)
+    void load4() {
+        imageview.setImageResource(R.drawable.android4);
+    }
+
+    @Bullet(sequence = 5, delay = 500)
+    void load5() {
+        imageview.setImageResource(R.drawable.android5);
+    }
+
+    @Bullet(sequence = 6, delay = 500)
+    void load6() {
+        imageview.setImageResource(R.drawable.android6);
+        Gunner.cancel(this);
         Gunner.shoot(this);
     }
 
-    @Bullet(sequence = 1, delay = 1000)
-    private void load1() {
-        Log.v("TAG", "load1");
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Gunner.shoot(this);
     }
 
-    @Bullet(sequence = 2, delay = 2000)
-    private void load2() {
-        Log.v("TAG", "load2");
-    }
-
-    @Bullet(sequence = 3, delay = 3000)
-    void load3() {
-        Log.v("TAG", "load3");
+    @Override
+    protected void onStop() {
+        super.onStop();
         Gunner.cancel(this);
-    }
-
-    @Bullet(sequence = 4)
-    void load4() {
-        Log.v("TAG", "load4");
     }
 }
